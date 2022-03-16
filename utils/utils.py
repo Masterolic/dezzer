0e420cfb9ce0fba7b286ab7c68d490e5e675792e
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-16 21:09:14 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -7,12 +7,18 @@ from helpers.db_help import initialize_db
 from libtmux import Server as tmux_server
 from .converter_bytes import convert_bytes_to
 from deezloader.libutils.utils import var_excape
-from deezloader.deezloader.deezer_settings import qualities
+from deezloader.deezloader.deezer_settings import qualities as dee_qualities
+from deezloader.spotloader.spotify_settings import qualities as spo_qualities
+
+from logging import (
+	getLogger, FileHandler,
+	Formatter, Logger
+)
 
 from configs.bot_settings import (
 	output_songs, supported_link, output_shazam,
-	log_errors, log_downloads,
-	log_uploads, logs_path
+	logs_path, logger_names,
+	log_uploads, log_downloads
 )
 
 from os import (
@@ -23,11 +29,19 @@ from os import (
 
 from os.path import (
 	getsize, join,
-	islink, isdir, exists
+	islink, isdir
+)
+
+__qualities_dee = list(
+	dee_qualities.keys()
+)
+
+__qualities_spo = list(
+	spo_qualities.keys()
 )
 
 def check_config_file(config):
-	if not "token" in config['deez_login']:
+	if not "arl" in config['deez_login']:
 		print("Something went wrong with the login token in the configuration file")
 		exit()
 
@@ -53,6 +67,11 @@ def check_config_file(config):
 		print("Something went wrong with acrcloud in the configuration file")
 		exit()
 
+def get_netloc(link):
+	netloc = urlparse(link).netloc
+
+	return netloc
+
 def is_supported_link(link):
 	is_supported = True
 	netloc = urlparse(link).netloc
@@ -65,43 +84,61 @@ def is_supported_link(link):
 
 	return is_supported
 
-def set_path(tag, song_quality, file_format, method):
-	album = tag['album']
-	album = __var_excape(album)
-
-	if method == 0:
-		discnum = tag['discnum']
-		tracknum = tag['tracknum']
-		song_name = f"{album} CD {discnum} TRACK {tracknum}"
-
-	elif method == 1:
-		artist = __var_excape(tag['artist'])
-		music = __var_excape(tag['music'])
-		song_name = f"{music} - {artist}"
-
-	elif method == 2:
-		artist = __var_excape(tag['artist'])
-		music = __var_excape(tag['music'])
-		isrc = tag['isrc']
-		song_name = f"{music} - {artist} [{isrc}]"
-
+def __get_tronc(string):
 	l_encoded = len(
-		song_name.encode()
+		string.encode()
 	)
 
 	if l_encoded > 242:
-		n_tronc = l_encoded - 242
-		n_tronc = len(song_name) - n_tronc
+		n_tronc = len(string) - l_encoded - 242
 	else:
 		n_tronc = 242
 
-	song_path = f"{song_name[:n_tronc]}"
-	song_path += f" ({song_quality}){file_format}"
+	return n_tronc
+
+def set_path(song_metadata, song_quality, file_format, method_save):
+	album = var_excape(song_metadata['album'])
+	artist = var_excape(song_metadata['artist'])
+	music = var_excape(song_metadata['music'])
+
+	if method_save == 0:
+		discnum = song_metadata['discnum']
+		tracknum = song_metadata['tracknum']
+		song_name = f"{album} CD {discnum} TRACK {tracknum}"
+
+	elif method_save == 1:
+		song_name = f"{music} - {artist}"
+
+	elif method_save == 2:
+		isrc = song_metadata['isrc']
+		song_name = f"{music} - {artist} [{isrc}]"
+
+	elif method_save == 3:
+		discnum = song_metadata['discnum']
+		tracknum = song_metadata['tracknum']
+		song_name = f"{discnum}:{tracknum} - {music} - {artist}"
+
+	n_tronc = __get_tronc(song_name)
+	song_path = f"{song_name[:n_tronc]} ({song_quality}){file_format}"
+
 	return song_path
 
-def get_quality(quality):
-	chosen = qualities[quality]
+def get_quality_dee_s_quality(dee_quality):
+	chosen = dee_qualities[dee_quality]
 	s_quality = chosen['s_quality']
+
+	return s_quality
+
+def get_quality_spo(dee_quality):
+	index = __qualities_dee.index(dee_quality)
+	chosen = __qualities_spo[index]
+
+	return chosen
+
+def get_quality_spo_s_quality(spo_quality):
+	chosen = spo_qualities[spo_quality]
+	s_quality = chosen['s_quality']
+
 	return s_quality
 
 def get_url_path(link):
@@ -109,14 +146,17 @@ def get_url_path(link):
 	path = parsed.path
 	s_path = path.split("/")
 	path = f"{s_path[-2]}/{s_path[-1]}"
+
 	return path
 
 def my_round(num):
 	rounded = round(num, 2)
+
 	return rounded
 
 def get_image_bytes(image_url):
 	content = req_get(image_url).content
+
 	return content
 
 def get_avalaible_disk_space():
@@ -124,6 +164,7 @@ def get_avalaible_disk_space():
 	total = convert_bytes_to(total, "gb")
 	used = convert_bytes_to(used, "gb")
 	free = convert_bytes_to(free, "gb")
+
 	return free
 
 def get_download_dir_size():
@@ -137,6 +178,7 @@ def get_download_dir_size():
 				total_size += getsize(fp)
 
 	total_size = convert_bytes_to(total_size, "gb")
+
 	return total_size
 
 def clear_download_dir():
@@ -167,15 +209,22 @@ def create_log_dir():
 	if not isdir(logs_path):
 		mkdir(logs_path)
 
-	if not exists(log_downloads):
-		f = open(log_downloads, "w")
-		f.write("")
-		f.close()
+def logging_bot() -> list[Logger]:
+	formatter = Formatter(
+		"%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+	)
 
-	if not exists(log_uploads):
-		f = open(log_uploads, "w")
-		f.write("")
-		f.close()
+	loggers = []
+
+	for logger, level, log_path in logger_names:
+		fu = FileHandler(log_path)
+		fu.setFormatter(formatter)
+		c_logger = getLogger(logger)
+		c_logger.setLevel(level)
+		c_logger.addHandler(fu)
+		loggers.append(c_logger)
+
+	return loggers
 
 def check_config_bot():
 	initialize_db()
@@ -186,11 +235,14 @@ def check_config_bot():
 def get_size(f, size) -> float:
 	b_size = getsize(f)
 	mb_size = convert_bytes_to(b_size, size)
+
 	return mb_size
 
 def show_menu():
 	print("1): TEST MODE")
 	print("2): COOL MODE")
+	print("3): TEST MODE (NO ZIP)")
+	print("4): COOL MODE (NO ZIP)")
 
 	ans = input("How to use it?: ")
 
@@ -198,6 +250,10 @@ def show_menu():
 		choice = 1
 	elif ans == "2":
 		choice = 2
+	elif ans == "3":
+		choice = 3
+	elif ans == "4":
+		choice = 4
 	else:
 		exit()
 
@@ -232,7 +288,7 @@ def create_tmux():
 
 	pan_top_right = window.split_window(vertical = False)
 	pan_top_right.send_keys(cclear)
-	pan_top_right.send_keys(f"tail -f {log_errors}")
+	pan_top_right.send_keys(f"tail -f {log_downloads}")
 	pan_bot_left = pan_top_right.split_window(vertical = True)
 	pan_bot_left.send_keys(cclear)
 	pan_bot_left.send_keys(f"tail -n2 -f {log_uploads}")
